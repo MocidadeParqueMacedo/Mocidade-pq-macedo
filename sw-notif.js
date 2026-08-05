4fc55db0ce9f3eba3da28829a771099df76019a5
MocidadeParqueMacedo <rdj.parquemacedo@gmail.com>
2026-08-04 22:14:12 -0300

    Update sw-notif.js

--- sw-notif.js
+++ sw-notif.js
@@ -1,13 +1,18 @@
+// v2 - com icone e skipWaiting
+self.addEventListener('install', function() { self.skipWaiting(); });
+self.addEventListener('activate', function(e) { e.waitUntil(clients.claim()); });
+
 self.addEventListener('push', function(e) {
   var data = {};
   try { data = e.data.json(); } catch(_) {
     data = { title: e.data ? e.data.text() : 'Notificacao' };
   }
+  var iconUrl = 'https://mocidadeparquemacedo.github.io/Mocidade-pq-macedo/icon-192x192.png';
   e.waitUntil(
     self.registration.showNotification(data.title || 'Reuniao de Jovens', {
       body: data.body || '',
-      icon: 'https://mocidadeparquemacedo.github.io/Mocidade-pq-macedo/icon-192x192.png',
-      badge: 'https://mocidadeparquemacedo.github.io/Mocidade-pq-macedo/icon-192x192.png',
+      icon: iconUrl,
+      badge: iconUrl,
       requireInteraction: true,
       tag: data.tag || 'evento',
       vibrate: [200, 100, 200]
